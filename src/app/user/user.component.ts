780d0c23689494823ee1c0d0ac8820241c14d586
Gabriel Morais <gmorais1994@gmail.com>
2018-04-11 22:49:17 -0300

    estilizacao v2 / logica de checar acertos (incompleto)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -27,6 +27,7 @@ export class UserComponent implements OnInit{
       { answer: 'Fala da história da empresa para criar credibilidade e, em seguida, explica a solução detalhadamente.', value: 0 },
       { answer: 'Fala da história da empresa para criar credibilidade, explica detalhadamente a solução, contextualizando qual problema ela resolve.', value: 0 },
     ],
+    id: 'question1',
   },
 
     {question: '2. Como vendedor:',
@@ -36,6 +37,7 @@ export class UserComponent implements OnInit{
       { answer: 'Você busca entender o problema dos clientes que não precisam da solução/produto, convence-os de que precisam e consegue vender.', value: 0 },
       { answer: 'Você entende que os clientes se incomodam com vendedores e espera eles demonstrarem interesse na sua solução/produto para fazer a venda; de modo a não incomodar os clientes com abordagens frias.', value: 0 },
     ],
+    id: 'question2',
   },
 
     {question: '3. Quando você desiste de um prospect?',
@@ -45,6 +47,7 @@ export class UserComponent implements OnInit{
       { answer: 'Eu nunca desisto do prospect. Quando ele me diz um "Não", eu agendo para voltar a falar com ele depois de um tempo.', value: 0 },
       { answer: 'Eu sou razoável. Eu entro em contato com o prospect e caso ele não responda, eu o respeito e não faço novo contato.', value: 0 },
     ],
+    id: 'question3',
   },
 
     {question: '4. Assinale a alternativa mais correta:',
@@ -54,15 +57,17 @@ export class UserComponent implements OnInit{
       { answer: 'As leads oriundas do Inbound Marketing, podem não ser suficientes para gerar os resultados que o negócio precisa, por isso, é importante também buscar de forma ativa possíveis clientes', value: 10 },
       { answer: 'A captação ativa de leads é a estratégia que traz mais resultado, entretanto, é importante buscar também fazer um bom inbound marketing e entregar um excelente produto para os clientes para que eles façam indicações e evangelizem o produto.', value: 0 },
     ],
+    id: 'question4',
   },
 
-    {question: '5. Alessandra é a melhor vendedora na Goal Hunter, conhecida por trabalhar com foco no cliente. Seu gestor, no mês passado, escreveu uma recomendação para Alessandra no LinkedIn. Como você completaria o texto conforme a ordem acima? “Aprendi, na teoria e prática, que um bom vendedor ao iniciar uma negociação necessita compreender de forma clara os objetivos que _______ possui a curto, médio e longo prazo. Alessandra, além disso, apresenta _________ para sanar as dores do lead, estabelece relações ________, criando valor e aumentando o engajamento. Por conta disso, ________ realiza a venda de produtos desnecessários para aumentar a receita e alcançar o 1º lugar no ranking de vendas da Goal Hunter, pois está constantemente focado nos objetivos _________. Recomendo Alessandra, mas espero que ela faça parte do meu time por muito tempo.”',
+    {question: '5. Alessandra é a melhor vendedora na Goal Hunter, conhecida por trabalhar com foco no cliente. Seu gestor, no mês passado, escreveu uma recomendação para Alessandra no LinkedIn. Como você completaria o texto conforme a ordem abaixo?\n\n“Aprendi, na teoria e prática, que um bom vendedor ao iniciar uma negociação necessita compreender de forma clara os objetivos que _______ possui a curto, médio e longo prazo. Alessandra, além disso, apresenta _________ para sanar as dores do lead, estabelece relações ________, criando valor e aumentando o engajamento. Por conta disso, ________ realiza a venda de produtos desnecessários para aumentar a receita e alcançar o 1º lugar no ranking de vendas da Goal Hunter, pois está constantemente focado nos objetivos _________. Recomendo Alessandra, mas espero que ela faça parte do meu time por muito tempo.”',
     alternatives: [
       { answer: 'O lead / a solução ideal / humanizadas / nunca / do lead', value: 10 },
       { answer: 'A empresa / o maior desconto / perde-ganha / sempre / da empresa', value: 0 },
       { answer: 'A empresa / diversos produtos / perde-perde / na maioria das vezes / da empresa', value: 0 },
       { answer: 'O vendedor / as soluções ideiais / humanizadas / às vezes / dela', value: 0 },
     ],
+    id: 'question5',
   },
 
     {question: '6. Determine se é Verdadeiro (V) ou Falso (F): \nI) Na estratégia de Inbound Sales, o conteúdo – que pode ser blog post, vídeo, webinar, etc – é a principal ferramenta para a atração do lead \nII) A estratégia de Outbound Sales é necessariamente ativa',
@@ -73,6 +78,7 @@ export class UserComponent implements OnInit{
       { answer: 'F - V - V - F - V', value: 0 },
       { answer: 'V - V - F - V - F', value: 0 },
     ],
+    id: 'question6',
   },
 
     {question: '7. Sabemos que, quanto mais rápido o contato com um lead após ocorrer a “levantada de mão”, maiores são as chances de conversão em cliente. Você trabalha em uma empresa com a metodologia Inbound e acabou de chegar um lead no seu CRM que solicitou contato há 1 minuto atrás. Você sabe o nome e o telefone dele, qual é a sua próxima ação?',
@@ -83,6 +89,7 @@ export class UserComponent implements OnInit{
       { answer: 'Não ligar, pois você não tem informações suficientes sobre o lead no momento', value: 0 },
       { answer: 'Ligar imediatamente e descobrir as demais informações durante a ligação', value: 0 },
     ],
+    id: 'question7',
   },
 
     {question: '8. Determine se é Verdadeiro (V) ou Falso (F).\nA venda para leads chamados “Ideal Customer Profile” ocasiona a médio e longo prazo:\nI) Aumento da taxa de churn\nII) Menor necessidade de qualificação\nIII) Um cliente mais satisfeito\nIV) Aumento do ciclo de vendas\nV) Maior engajamento',
@@ -93,6 +100,7 @@ export class UserComponent implements OnInit{
       { answer: 'V - F - V - V - F', value: 0 },
       { answer: 'F - F - V - F - V', value: 10 },
     ],
+    id: 'question8',
   },
 
     {question: '9. Qual das duas abordagens de e-mail parece fazer mais sentido para você e por quê?\nI) Assunto: Apresentação Gabriel - Olá, Pedro, Aqui é o Gabriel, da Goal Hunter. Nós ajudamos empresas a encontrar talentos nas áreas de vendas e CS, e vi que possuem várias semelhanças com empresas de software que atendemos hoje. Outros CEOs que ajudo atualmente estão especialmente preocupados em encontrar os melhores profissionais de mercado de forma mais prática. É o caso de vocês? Faz sentido conversar 10 minutos sobre o assunto em algum horário após as 16:00 de quinta ou sexta?\nII) Assunto: Como melhorar seu recrutamento! Olá Pedro, Aqui é o Gabriel, da Goal Hunter. Nós ajudamos empresas a encontrar talentos nas áreas de vendas e CS, e vi que possuem várias semelhanças com empresas de software que atendemos hoje. Outros CEOs que ajudo atualmente estão especialmente preocupados em encontrar os melhores profissionais de mercado de forma mais prática. Por favor me deixe saber se é um assunto importante para vocês. Obrigado!',
@@ -102,6 +110,7 @@ export class UserComponent implements OnInit{
       { answer: 'A II, pois o assunto é mais assetivo e a conclusão é menos invasiva', value: 0 },
       { answer: 'A II, pois, apesar do assunto menos efetivo, configura-se em uma metodologia consultiva, alinhando-se às tendências de mercado', value: 10 },
     ],
+    id: 'question9',
   },
 
     {question: '10. Você está finalizando uma call com o lead e precisa decidir qual o próximo passo que realizará para avançar o deal, você tem duas opções:\nI) Ainda na ligação, agenda data e hora da próxima call e logo após envia um e-mail com breve resumo do que conversaram.\nII) Informa o lead que irá enviar um e-mail com todas as informações do produto, encerra a call e ao escrever o e-mail sugere uma data e horário para a tirar as dúvidas do lead e avançar o deal.',
@@ -109,6 +118,7 @@ export class UserComponent implements OnInit{
       { answer: 'A I, pois com a próxima ação agendada, tenho maior controle da negociação e favorecimento para que meu pipeline seja verdadeiro, consistente e previsível', value: 10 },
       { answer: 'A II, pois, se o lead tiver alguma dúvida, pode consultar o meu e-mail com as informações detalhadas e o deixarei à vontade para marcar a próxima conversa comigo; assim não irei atrapalhá-lo, vou gerar relevância com as informações e isolar as possíveis objeções', value: 0 },
     ],
+    id: 'question10',
   },
 
 
@@ -120,6 +130,7 @@ export class UserComponent implements OnInit{
       { answer: 'São os prospects da sua empresa.', value: 0 },
       { answer: 'São os clientes que já compraram de sua empresa.', value: 0 },
     ],
+    id: 'question11',
   },
 
   ];
@@ -153,6 +164,18 @@ export class UserComponent implements OnInit{
   }
 
   sumValue(){
-
+    let radius = document.querySelectorAll(".subtotal:checked");
+    if(radius.length == 11){
+      console.log(radius);
+      for (let index = 0; index < radius.length; index++) {
+        console.log(radius[index].value);
+        if(radius[index].value == 0){
+          radius.splice(index, 1);
+        }
+      }
+      console.log(radius);
+    } else {
+      console.log('error');
+    }
   }
 }
