6cdf7267a5c61eba2ba4dc7fcbb660ce769470d6
Breno Franca <bre.sfranca@gmail.com>
2018-04-18 12:42:39 -0300

    Inserindo Senioridade

--- src/app/core/user.model.ts
+++ src/app/core/user.model.ts
@@ -1,13 +1,9 @@
 export class FirebaseUserModel {
-  image: string;
   name: string;
   provider: string;
-  email: string;
 
   constructor(){
-    this.image = "";
     this.name = "";
     this.provider = "";
-    this.email = "";
   }
 }
